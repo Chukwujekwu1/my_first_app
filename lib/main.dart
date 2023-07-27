7bb7f184cb18b3046cb1d2db932e1f60861aeaa1
officialmihalis <ongurd5@gmail.com>
2023-07-26 20:11:48 -0700

    image

--- lib/main.dart
+++ lib/main.dart
@@ -20,30 +20,42 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
+
+
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: Colors.red,
-        body: Center(
-          child: Column(
-            children: [
-              Text('Welcome', style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 55,
-                    fontWeight: FontWeight.bold),),
-             SizedBox(height:20.00,),
-              Text(
-                'To Mapp Blog',
-                style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold),
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
-        ));
+            ),
+         const   Text(
+              'To Mapp Blog',
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          const  SizedBox(height: 20.0),
+            Image.asset("images/3d-fluency-launch.png"),
+          ],
+        ),
+      ),
+    );
   }
 }
