59f4cac691ff3126d346c86a77fb4df7e8871a70
officialmihalis <ongurd5@gmail.com>
2023-07-27 18:34:07 -0700

    done

--- lib/description_page.dart
+++ lib/description_page.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
 
+ String backonText = "Bacon ipsum dolor amet ground round pork doner, landjaeger shoulder biltong ribeye cow pork chop picanha kielbasa hamburger shankle fatback. Sirloin capicola short ribs cow, short loin pork loin pancetta fatback ham hock. Picanha cow pork ham fatback short ribs kielbasa tongue tenderloin cupim rump flank turducken. Pork belly landjaeger cow sirloin turducken swine flank capicola ham hock cupim drumstick. Ham salami cupim porchetta sirloin shankle turducken pancetta burgdoggen drumstick bresaola chuck strip steak meatloaf short loin. Porchetta filet mignon swine spare ribs leberkas picanha. Brisket tail rump, ball tip cupim chislic bresaola buffalo strip steak pastrami shankle. Ham hock chuck hamburger venison. Pork belly turkey short ribs tongue picanha ground round ball tip beef meatloaf. Chicken bresaola meatloaf chislic, sirloin pork cow ham hock drumstick filet mignon turducken beef ribs. Leberkas jowl andouille capicola ground round ball tip, brisket spare ribs cow bresaola pastrami. Landjaeger prosciutto doner, tongue bresaola filet mignon pork chop pork. Hamburger ham landjaeger, bresaola turkey ground round chislic pork belly chicken. Capicola turducken porchetta ground round ball tip. Meatball shank shankle cupim turducken. Shankle beef buffalo pancetta beef ribs biltong hamburger fatback jerky ham. Fatback pork chop chicken, chislic meatball flank ham. Beef ribs jerky leberkas bresaola t-bone pork chop pork loin sirloin tri-tip picanha andouille. Picanha boudin short ribs ham bresaola salami rump burgdoggen cow prosciutto pastrami. Chislic pork chop leberkas short ribs hamburger short loin doner frankfurter brisket shoulder ham hock cupim sausage. Pastrami leberkas sirloin fatback. Doner landjaeger pancetta pork chop kevin. Leberkas kevin tail filet mignon bacon. Capicola salami leberkas chislic tongue beef ribs pancetta swine ground round pig. Buffalo corned beef landjaeger venison tongue flank, ham ground round. Drumstick buffalo fatback, ham doner kielbasa pig prosciutto jowl. Prosciutto brisket pork belly ham hock spare ribs pig venison biltong burgdoggen short loin short ribs pastrami chuck swine. Short ribs turkey kielbasa, rump prosciutto ham flank doner sausage beef t-bone turducken capicola. Pork chop beef ribs frankfurter strip steak short ribs filet mignon beef cow.";
 
 class DescriptionPage extends StatelessWidget {
-  const DescriptionPage({super.key});
+  const DescriptionPage({
+    super.key,
+    required this.title,
+    required this.imagePath,
+  });
+  final String title;
+  final String imagePath;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
+        title: Text(title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Image.asset(
+                imagePath,
+                width: 300,
+                height: 300,
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 25.0,
+              ),
+              //! backon
+              Text(
+                backonText,
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,16 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app_1/description_page.dart';
+import 'package:my_first_app_1/main.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color background = Colors.white;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( ),
-      drawer:const SafeArea(
-        child:  Drawer( 
-          child: Column(),
+      backgroundColor: background,
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+              if (background == Colors.white) {
+                background = Colors.red;
+              } else {
+                background = Colors.white;
+              }  
+              },);
+            },
+            icon: const Icon(Icons.color_lens,),
+          ),
+        ],
+      ),
+      drawer: SafeArea(
+        child: Drawer(
+          child: Column(
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                ),
+                child: ListTile(
+                  title: Text(
+                    "Michael",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                onTap: () {},
+                leading: const Icon(Icons.settings),
+                title: const Text(
+                  "Settings",
+                ),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return const LoginPage();
+                      },
+                    ),
+                  );
+                },
+                leading: const Icon(Icons.logout),
+                title: const Text(
+                  "Logout",
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       body: SingleChildScrollView(
@@ -62,12 +124,16 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
+            //!! first
             GestureDetector(
               onTap: () {
                 Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (BuildContext context) {
-                      return const DescriptionPage();
+                      return const DescriptionPage(
+                        title: 'How to get rich',
+                        imagePath: "images/bitcoin.png",
+                      );
                     },
                   ),
                 );
@@ -106,12 +172,18 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
+
+            //! second
+
             GestureDetector(
               onTap: () {
                 Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (BuildContext context) {
-                      return Container();
+                      return const DescriptionPage(
+                        title: "How to make 10k/month",
+                        imagePath: 'images/rocketride.png',
+                      );
                     },
                   ),
                 );
@@ -149,12 +221,18 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
+
+            //! third
+
             GestureDetector(
               onTap: () {
                 Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (BuildContext context) {
-                      return Container();
+                      return const DescriptionPage(
+                        title: "Real Estate investment",
+                        imagePath: 'images/rocketdab.png',
+                      );
                     },
                   ),
                 );
@@ -187,7 +265,59 @@ class HomePage extends StatelessWidget {
                         height: 300,
                       ),
                       const ListTile(
-                        title: Text("How to make 10k/month"),
+                        title: Text("Real Estate investment"),
+                        trailing: Icon(Icons.arrow_forward_ios_rounded),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            //! fourth
+
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return const DescriptionPage(
+                        title: "Stock Market",
+                        imagePath: 'images/rocketman.png',
+                      );
+                    },
+                  ),
+                );
+              },
+              child: Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10.00,
+                  vertical: 5.00,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      spreadRadius: 2,
+                      blurRadius: 3,
+                      offset: Offset(2, 2),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        'images/rocketman.png',
+                        width: 280,
+                        height: 300,
+                      ),
+                      const ListTile(
+                        title: Text("Stock Market"),
                         trailing: Icon(Icons.arrow_forward_ios_rounded),
                       )
                     ],
